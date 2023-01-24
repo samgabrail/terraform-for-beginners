56dbc94745bdf66e4c52e565489874236e68bd11
Sam <8408317+samgabrail@users.noreply.github.com>
2023-01-24 22:04:17 +0000

    more fixes

--- 08-Modules/modules/tls/main.tf
+++
@@ -1,4 +0,0 @@
-resource "tls_private_key" "mykey" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
--- 09-A-Web-Server/main.tf
+++ 09-A-Web-Server/main.tf
@@ -38,7 +38,7 @@ resource "aws_key_pair" "mykey" {
 
 resource "aws_instance" "webserver" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+  instance_type = var.my_instance_type
   key_name      = aws_key_pair.mykey.key_name
   vpc_security_group_ids = [aws_security_group.security_group1.id]
   user_data                   = file("install_libraries.sh")
